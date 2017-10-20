759ec970e877861b0bdd0e25711f50c0e5ceebc3
i008 <kubacieslik@gmail.com>
2017-10-20 20:10:49 +0200

    removing neptune_requirements

--- run_neptune_command.sh
+++ run_neptune_command.sh
@@ -1,6 +1,6 @@
 neptune send experiment_manager.py \
 --config experiment_config.yaml \
---pip-requirements-file neptune_requirements.txt \
+--pip-requirements-file requirements.txt \
 --project-key CDIS \
 --environment keras-2.0-gpu-py3 \
 --worker gcp-gpu-medium \
